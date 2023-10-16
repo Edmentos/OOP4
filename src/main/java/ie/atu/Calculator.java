b1e0b985bdd2f92768bffa3a70759a336d825e7b
Edmond <G00414989@atu.ie>
2023-10-16 17:34:51 +0100

    Added a new method called multiply

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
        add();
        delete();
        divide();
+       multiply();
     }
 
     public static void add() // This code makes it so that the calculator can add
@@ -45,6 +46,19 @@ public class Calculator {
         int secondNumber = inputs.nextInt();
 
         int total = firstNumber / secondNumber; // First number / the second number = total
+        System.out.println("The total is: " + total);
+    }
+
+    public static void multiply() // This code makes it so that the calculator can divide
+    {
+        System.out.println("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please input your second number: ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber; // First number * the second number = total
         System.out.println("The total is: " + total);
     }
 
